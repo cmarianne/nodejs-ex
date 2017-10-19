f347fd8feeee3bce8304b55ff07936cf2b90ec8d
cmarianne <32707895+cmarianne@users.noreply.github.com>
2017-10-19 14:07:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
       mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
       mongoPassword = process.env[mongoServiceName + '_PASSWORD']
       mongoUser = process.env[mongoServiceName + '_USER'];
-
+console.log (mongoHost " + " mongoPort );
   if (mongoHost && mongoPort && mongoDatabase) {
     mongoURLLabel = mongoURL = 'mongodb://';
     if (mongoUser && mongoPassword) {
